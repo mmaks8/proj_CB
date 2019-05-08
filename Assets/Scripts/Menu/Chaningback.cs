037c6d3a19cab22ede03c2a4af8d0a757bac371a
ymarquina <ymarquina@users.noreply.github.com>
2019-05-08 12:55:05 -0700

    Timer added and mini map

---
+++ Assets/Scripts/Menu/Chaningback.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaningback : MonoBehaviour
+{
+    public void Nexxt()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DRY_PLANET");
+    }
+
+}
