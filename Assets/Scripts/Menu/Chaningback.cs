6e1ba07aa069b31e647c6b938e1a686434365c01
ymarquina <ymarquina@users.noreply.github.com>
2019-05-09 00:26:58 -0700

    minimap

--- Assets/Scripts/Menu/Chaningback.cs
+++ Assets/Scripts/Menu/Chaningback.cs
@@ -6,7 +6,7 @@ public class Chaningback : MonoBehaviour
 {
     public void Nexxt()
     {
-
+        Timer.startTime += 10.0f;
         UnityEngine.SceneManagement.SceneManager.LoadScene("DRY_PLANET");
     }
 
--- Assets/Scripts/Menu/Timer.cs
+++ Assets/Scripts/Menu/Timer.cs
@@ -2,46 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public Text timertext;
-    public float startTime;
+    static  public float startTime = 50.0f;
     public float t;
+    //public string Scenemane;
+  //  public string currentscene;
     //public float startTime = 120.0f;
     // private bool finnish = false;
 
 
-    //public void start()
-    //
-    // {
-    //   startTime = 120.0f;
-    // }
+  
 
-    //Update is called once per frame
-    //public void star()
+       // public void Start()
 
-    //{
-
-        public void Start()
-        {
-            startTime = 75.0f;
-        }
+      //  {
+      //  currentscene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+       // startTime = 10.0f;
+      // Scenemane = currentscene.name;
+      //  }
 
 
     public void Update()
    
         {
-
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene() == UnityEngine.SceneManagement.SceneManager.GetSceneByName("DRY_PLANET") || UnityEngine.SceneManagement.SceneManager.GetActiveScene() == UnityEngine.SceneManagement.SceneManager.GetSceneByName("TimesUP"))
+        {
+           
             t = startTime - Time.time;
-
+            //startTime += 10.0f;
             if (t < 0.0)
             {
-
+               // startTime += 60.0f
                 timertext.color = Color.red;
-                startTime += 60.0f;
+              // startTime += 60.0f;
                 UnityEngine.SceneManagement.SceneManager.LoadScene("TimesUP");
                 // startTime += 5.0f;
                 //t = 5.0f;
@@ -54,8 +51,8 @@ public class Timer : MonoBehaviour
             string seconds = (t % 60).ToString("f1");
 
             timertext.text = minutes + ":" + seconds;
-            
 
+        }
         
 
         // public void ManageScene()
